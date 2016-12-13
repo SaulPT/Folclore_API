9c67906b5f93e55dbd457e6d88dbf6332403cc77
SaulPT <saulezeq@hotmail.com>
2016-12-13 14:53:14 +0000

    PUT de grupo quase funcional

--- api/controllers/GrupoController.php
+++ api/controllers/GrupoController.php
@@ -13,6 +13,7 @@ class GrupoController extends \yii\rest\ActiveController
 
     public function actions()
     {
+        /////////////////////////
         //DESABILITAR TODAS AS FUNÇÕES CRUD EXCEPTO O VIEW (GET)
         $actions = parent::actions();
         unset($actions['index'], $actions['create'], $actions['delete'], $actions['update'], $actions['options']);
